a47aaab41147a73ca59e2416973b47e925fd7c62
Suer-Tech <suer2505@gmail.com>
2023-12-28 19:37:20 +0500

    Correctly create_db in db_crud.py

--- db_crud.py
+++ db_crud.py
@@ -3,15 +3,17 @@ from conf_db import conn_params, db_create_params
 
 
 async def create_db(db_name):
-    conn = await asyncpg.connect(**db_create_params)
-    try:
-        conn.autocommit = True
-        cur = await conn.cursor()
+    # Устанавливаем параметры подключения к базе данных
+    create_conn_params = db_create_params.copy()
+    create_conn_params["database"] = "postgres"  # Подключаемся к системной базе данных PostgreSQL
 
-        await cur.execute(f'CREATE DATABASE {db_name}')
+    conn = await asyncpg.connect(**create_conn_params)
+
+    try:
+        await conn.execute(f'CREATE DATABASE {db_name}')
         print('success create db....')
 
-    except asyncpg.Error as e:
+    except asyncpg.exceptions.PostgresError as e:
         print(f"Ошибка при создании базы данных: {e}")
 
     finally:
@@ -32,20 +34,20 @@ class Crypto:
     async def create_table(self):
         conn = await asyncpg.connect(**conn_params)
         try:
-            cur = await conn.cursor()
-            await cur.execute(f'''
-                CREATE TABLE IF NOT EXISTS {self.symbol}_table (
-                    id SERIAL PRIMARY KEY,
-                    symbol VARCHAR(50),
-                    timestamp BIGINT,
-                    low_price DECIMAL,
-                    high_price DECIMAL,
-                    volume DECIMAL
-                )
-            ''')
-            print(f'success create table {self.symbol}_table')
+            async with conn.transaction():
+                await conn.execute(f'''
+                    CREATE TABLE IF NOT EXISTS {self.symbol}_table (
+                        id SERIAL PRIMARY KEY,
+                        symbol VARCHAR(50),
+                        timestamp BIGINT,
+                        low_price DECIMAL,
+                        high_price DECIMAL,
+                        volume DECIMAL
+                    )
+                ''')
+                print(f'success create table {self.symbol}_table')
 
-        except asyncpg.Error as e:
+        except asyncpg.exceptions.PostgresError as e:
             print(f"Ошибка при создании таблицы: {e}")
 
         finally:
@@ -61,15 +63,14 @@ class Crypto:
 
         conn = await asyncpg.connect(**conn_params)
         try:
-            cur = await conn.cursor()
+            async with conn.transaction():
+                await conn.execute(f'''
+                    INSERT INTO {self.symbol}_table (symbol, timestamp, low_price, high_price, volume)
+                    VALUES ($1, $2, $3, $4, $5)
+                ''', (self.symbol, self.timestamp, self.low_price, self.high_price, self.volume))
+                print(f'success add data on table {self.symbol}_table')
 
-            await cur.execute(f'''
-                INSERT INTO {self.symbol}_table (symbol, timestamp, low_price, high_price, volume)
-                VALUES ($1, $2, $3, $4, $5)
-            ''', (self.symbol, self.timestamp, self.low_price, self.high_price, self.volume))
-            print(f'success add data on table {self.symbol}_table')
-
-        except asyncpg.Error as e:
+        except asyncpg.exceptions.PostgresError as e:
             print(f"Ошибка при загрузке данных в таблицу {self.symbol}_table: {e}")
 
         finally:
--- process.py
+++ process.py
@@ -1,17 +1,21 @@
+import asyncio
 from conf_db import db_name
 from db_crud import create_db, Crypto
 from price_api import get_kline_data, get_symbol_price_ticker
 
 limit_kline = 500
 
-create_db(db_name)
 
-cryptos = get_symbol_price_ticker()
+async def main():
+    await create_db(db_name)
 
-data = {symbol: get_kline_data(symbol, '1h', limit=limit_kline, startTime=None, endTime=None) for symbol in cryptos}
+    cryptos = await get_symbol_price_ticker()
 
-crypto_data = [Crypto(symbol, data_instance) for symbol, data_instance in data.items()]
+    if cryptos:
 
-tables = [crypto.create_table() for crypto in crypto_data]
+        data = {symbol: await get_kline_data(symbol, '1h', limit=limit_kline) for symbol in cryptos}
+        crypto_data = [Crypto(symbol, data_instance) for symbol, data_instance in data.items()]
+        tables = [await crypto.create_table() for crypto in crypto_data]
+        save = [await crypto_kline_data.save_to_database(i) for crypto_kline_data in crypto_data for i in range(limit_kline)]
 
-save = [crypto_kline_data.save_to_database(i) for crypto_kline_data in crypto_data for i in range(limit_kline)]
+asyncio.run(main())
