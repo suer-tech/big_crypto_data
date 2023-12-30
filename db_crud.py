3af26bf4a6ab46b307a192f5f2f1e4fabe2c4cfe
suer-tech <suer2505@gmail.com>
2023-12-30 15:53:13 +0300

    Add pool on connect to db in process and check attempt to db_crud

--- db_crud.py
+++ db_crud.py
@@ -1,5 +1,7 @@
+import asyncio
+
 import asyncpg
-from conf_db import conn_params, db_create_params
+from conf_db import db_create_params
 
 
 async def create_db(db_name):
@@ -31,49 +33,56 @@ class Crypto:
         self.high_price = None
         self.volume = None
 
-    async def create_table(self):
-        conn = await asyncpg.connect(**conn_params)
-        try:
-            async with conn.transaction():
-                await conn.execute(f'''
-                    CREATE TABLE IF NOT EXISTS _{self.symbol}_table (
-                        id SERIAL PRIMARY KEY,
-                        symbol VARCHAR(50),
-                        timestamp BIGINT,
-                        low_price DECIMAL,
-                        high_price DECIMAL,
-                        volume DECIMAL
-                    )
-                ''')
-                print(f'success create table _{self.symbol}_table')
+    def __str__(self):
+        return self.symbol
 
-        except asyncpg.exceptions.PostgresError as e:
-            print(f"Ошибка при создании таблицы: {e}")
+    async def create_table(self, pool):
+        max_retries = 10
+        async with pool.acquire() as connection:
+            for attempt in range(max_retries):
+                print(self.symbol)
+                print(f'Попытка подключения {attempt}')
+                try:
 
-        finally:
-            # Закрытие соединения
-            if conn is not None:
-                await conn.close()
+                    async with connection.transaction():
+                        await connection.execute(f'''
+                            CREATE TABLE IF NOT EXISTS _{self.symbol}_table (
+                                id SERIAL PRIMARY KEY,
+                                symbol VARCHAR(50),
+                                timestamp BIGINT,
+                                low_price DECIMAL,
+                                high_price DECIMAL,
+                                volume DECIMAL
+                            )
+                        ''')
+                        print(f'success create table _{self.symbol}_table')
+                        break
 
-    async def save_to_database(self, i):
-        self.timestamp = int(self.data[i][0])
-        self.low_price = float(self.data[i][3])
-        self.high_price = float(self.data[i][2])
-        self.volume = float(self.data[i][5])
+                except asyncpg.exceptions.PostgresError as e:
+                    print(f"Ошибка при создании таблицы: {e}")
 
-        conn = await asyncpg.connect(**conn_params)
-        try:
-            async with conn.transaction():
-                await conn.execute(f'''
-                    INSERT INTO _{self.symbol}_table (symbol, timestamp, low_price, high_price, volume)
-                    VALUES ($1, $2, $3, $4, $5)
-                ''', self.symbol, self.timestamp, self.low_price, self.high_price, self.volume)
-                print(f'success add data on table _{self.symbol}_table')
+                    if attempt == max_retries -1:
+                        raise ValueError('Максимальное количество попыток создания таблицы')
 
-        except asyncpg.exceptions.PostgresError as e:
-            print(f"Ошибка при загрузке данных в таблицу {self.symbol}_table: {e}")
+                    await asyncio.sleep(5)
 
-        finally:
-            # Закрытие соединения
-            if conn is not None:
-                await conn.close()
+    async def save_to_database(self, i, pool):
+        if i < len(self.data):
+            self.timestamp = int(self.data[i][0])
+            self.low_price = float(self.data[i][3])
+            self.high_price = float(self.data[i][2])
+            self.volume = float(self.data[i][5])
+        else:
+            print(f'Индекс {i} выходит за пределы длины списка {self.symbol}')
+
+        async with pool.acquire() as connection:
+            try:
+                async with connection.transaction():
+                    await connection.execute(f'''
+                        INSERT INTO _{self.symbol}_table (symbol, timestamp, low_price, high_price, volume)
+                        VALUES ($1, $2, $3, $4, $5)
+                    ''', self.symbol, self.timestamp, self.low_price, self.high_price, self.volume)
+                    print(f'success add data on table _{self.symbol}_table')
+
+            except asyncpg.exceptions.PostgresError as e:
+                print(f"Ошибка при загрузке данных в таблицу {self.symbol}_table: {e}")
--- process.py
+++ process.py
@@ -1,8 +1,12 @@
 import asyncio
 import aiohttp
+import asyncpg
+
 from conf_db import db_name
 from db_crud import create_db, Crypto
 from price_api import get_symbol_price_ticker, get_symbol_kline_data
+from conf_db import conn_params
+
 
 interval = '1h'
 limit_kline = 500
@@ -29,9 +33,11 @@ async def main():
 
         crypto_objects = [Crypto(symbol, kline_data) for crypto_kline_data in all_crypto_kline_data for symbol, kline_data in crypto_kline_data.items()]
 
+        pool = await asyncpg.create_pool(**conn_params)
+
         db_table_create_tasks = []
         for crypto_obj in crypto_objects:
-            crypto_table_create = crypto_obj.create_table()
+            crypto_table_create = crypto_obj.create_table(pool)
             db_table_create_tasks.append(crypto_table_create)
 
         await asyncio.gather(*db_table_create_tasks)
@@ -39,9 +45,11 @@ async def main():
         db_data_save_tasks = []
         for crypto_obj in crypto_objects:
             for i in range(limit_kline):
-                crypto_data_save = crypto_obj.save_to_database(i)
+                crypto_data_save = crypto_obj.save_to_database(i, pool)
                 db_data_save_tasks.append(crypto_data_save)
 
         await asyncio.gather(*db_data_save_tasks)
+
+        await pool.close()
 
 asyncio.run(main())
